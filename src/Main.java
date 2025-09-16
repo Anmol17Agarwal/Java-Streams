3fb3d1ed50c8f0571aeffe3e28daa64c2293afd1
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-09-16 22:32:54 +0530

    even number in a list

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +10,11 @@ public class Main {
         List<Integer> number = List.of(5,4,3,6,21,35,4,3,5,4,3);
         int max = number.stream().max(Integer::compare).orElseThrow();
         System.out.println(max);
+
+
+        //Find the even number in a list
+        List<Integer> even = number.stream().filter(n->n%2==0).collect(Collectors.toList());
+        System.out.println(even);
+
     }
 }
