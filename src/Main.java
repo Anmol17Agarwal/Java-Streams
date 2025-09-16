2639b9975182b7a16dd2b0a886692692f307b131
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-09-16 22:27:05 +0530

    maximum number in a list

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        //Find the maximum number in a list
+        List<Integer> number = List.of(5,4,3,6,21,35,4,3,5,4,3);
+        int max = number.stream().max(Integer::compare).orElseThrow();
+        System.out.println(max);
+    }
+}
